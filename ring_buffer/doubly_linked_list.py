d5f48750790c4e99b340a3568d263a67a66d984d
Ricardo Leite <ricardo.leitejr95@gmail.com>
2020-07-17 09:50:37 -0600

    Completed Ring Buffer MVP

---
+++ ring_buffer/doubly_linked_list.py
@@ -0,0 +1,153 @@
+"""
+Each ListNode holds a reference to its previous node
+as well as its next node in the List.
+"""
+
+
+class ListNode:
+    def __init__(self, value, prev=None, next=None):
+        self.prev = prev
+        self.value = value
+        self.next = next
+
+    def delete(self):
+        if self.prev:
+            self.prev.next = self.next
+        if self.next:
+            self.next.prev = self.prev
+
+
+"""
+Our doubly-linked list class. It holds references to 
+the list's head and tail nodes.
+"""
+
+
+class DoublyLinkedList:
+    def __init__(self, node=None):
+        self.head = node
+        self.tail = node
+        self.length = 1 if node is not None else 0
+
+    def __len__(self):
+        return self.length
+
+    """
+    Wraps the given value in a ListNode and inserts it 
+    as the new head of the list. Don't forget to handle 
+    the old head node's previous pointer accordingly.
+    """
+
+    def add_to_head(self, value):
+        new_node = ListNode(value, None, self.head)  # Instantiated new Node
+        if not self.head and not self.tail:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.head.prev = new_node
+            self.head = new_node
+        self.length += 1
+
+    """
+    Removes the List's current head node, making the
+    current head's next node the new head of the List.
+    Returns the value of the removed Node.
+    """
+
+    def remove_from_head(self):
+
+        value = self.head.value  # store value
+        self.delete(self.head)  # delete it
+        return value
+
+    """
+    Wraps the given value in a ListNode and inserts it 
+    as the new tail of the list. Don't forget to handle 
+    the old tail node's next pointer accordingly.
+    """
+
+    def add_to_tail(self, value):
+        new_node = ListNode(value, self.tail, None)
+        if not self.head and not self.tail:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+
+    """
+    Removes the List's current tail node, making the 
+    current tail's previous node the new tail of the List.
+    Returns the value of the removed Node.
+    """
+
+    def remove_from_tail(self):
+        value = self.tail.value
+        self.delete(self.tail)
+        return value
+
+    """
+    Removes the input node from its current spot in the 
+    List and inserts it as the new head node of the List.
+    """
+
+    def move_to_front(self, node):
+        if node is self.head:
+            return
+        value = node.value
+        self.delete(node)
+        self.add_to_head(value)
+
+    """
+    Removes the input node from its current spot in the 
+    List and inserts it as the new tail node of the List.
+    """
+
+    def move_to_end(self, node):
+        if node is self.tail:
+            return
+        value = node.value
+        self.delete(node)
+        self.add_to_tail(value)
+
+    """
+    Deletes the input node from the List, preserving the 
+    order of the other elements of the List.
+    """
+
+    def delete(self, node):
+        self.length -= 1
+        if self.head is self.tail:
+            self.head = None
+            self.tail = None
+        elif node is self.head:
+            self.head = node.next
+            node.delete()
+        elif node is self.tail:
+            self.tail = node.prev
+            node.delete()
+        else:
+            node.delete()
+
+    """
+    Finds and returns the maximum value of all the nodes 
+    in the List.
+    """
+
+    def get_max(self):
+        if not self.head:
+            return None
+
+        max_value = self.head.value
+        current = self.head
+        while current:
+            if current.value > max_value:
+                max_value = current.value
+            current = current.next
+        return max_value
+
+
+dll = DoublyLinkedList()
+dll.add_to_head(1)
+dll.add_to_head(2)
